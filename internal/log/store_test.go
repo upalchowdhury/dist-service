32431bc9f8da07558a23bd5a9c041a34ea124188
upalchowdhury <upal.chowdhury@outlook.com>
2022-08-16 08:42:57 -0400

    tests

--- internal/log/store_test.go
+++ internal/log/store_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/require"
+	"github.com/stretchr/testify/require"
 )
 
 var (
@@ -28,4 +29,98 @@ func TestStoreAppendRead(t *testing.T) {
 	s, err = newStore(f)
 	require.NoError(t, err)
 	testRead(t, s)
+}
+
+func testAppend(t *testing.T, s *Store) {
+	t.Helper()
+
+	for i := uint64(1); i < 4; i++ {
+		n, pos, err := s.Append(write)
+		require.NoError(t, err)
+		require.Equal(t, pos+n, width*i)
+
+	}
+}
+
+func testRead(t *testing.T, s *Store) {
+	t.Helper()
+	var pos uint64
+
+	for i := uint64(1); i < 4; i++ {
+		read, err := s.Read(pos)
+		require.NoError(t, err)
+		require.Equal(t, write, read)
+		pos += width
+	}
+}
+
+func testReadAt(t *testing.T, s *Store) {
+	t.Helper()
+
+	for i, off := uint64(1), int64(0); i < 4; i++ {
+		b := make([]byte, lenwidth)
+
+		n, err := s.ReadAt(b, off)
+
+		require.NoError(t, err)
+
+		require.Equal(t, lenwidth, n)
+		off += int64(n)
+
+		size := enc.Uint64(b)
+		b = make([]byte, lenwidth)
+		n, err := s.ReadAt(b, off)
+
+		require.NoError(t, err)
+
+		require.Equal(t, write, b)
+
+		require.Equal(t, int(size), n)
+
+		off += int64(n)
+
+	}
+}
+
+func TestStoreClose(t *testing.T) {
+
+	f, err := ioutil.TempFile("", "store_close_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+	s, err := newStore(f)
+
+	require.NoError(t, err)
+
+	_, _, err = s.Append(write)
+	require.NoError(t, err)
+
+	f, beforeSize, err := openFile(f.Name())
+	require.NoError(t, err)
+
+	err = s.Close()
+	require.NoError(t, err)
+
+	_, afterSize, err := openFile(f.Name())
+
+	require.NoError(t, err)
+	require.True(t, afterSize > beforeSize)
+}
+
+func openFile(name string) (file *os.File, size int64, err error) {
+	f, err := os.OpenFile(
+		name,
+		os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644,
+	)
+
+	if err != nil {
+		return nil, 0, err
+	}
+
+	fi, err := f.Stat()
+
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return f, fi.Size(), nil
 }
