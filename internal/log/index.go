38cf101ce2ac73989f0c31d9eb4843ab9a67bd20
upalchowdhury <upal.chowdhury@outlook.com>
2022-08-20 10:04:56 -0400

    adding segment

--- internal/log/index.go
+++ internal/log/index.go
@@ -4,7 +4,7 @@ import (
 	"io"
 	"os"
 
-	"github.com/tysontate/gommap"
+	"github.com/tysonmote/gommap"
 )
 
 var (
@@ -82,9 +82,25 @@ func (i *index) Read(in int64) (out uint32, pos uint64, err error) {
 		return 0, 0, io.EOF
 	}
 
-	out = enc.Uint32(i.mmap[pos : pos+offfWidth])
+	out = enc.Uint32(i.mmap[pos : pos+offWidth])
 	pos = enc.Uint64(i.mmap[pos+offWidth : pos+entWidth])
 
 	return out, pos, nil
+
+}
+
+func (i *index) Write(off uint32, pos uint64) error {
+	if uint64(len(i.mmap)) < i.size+entWidth {
+		return io.EOF
+	}
+	enc.PutUint32(i.mmap[i.size:i.size+offWidth], off)
+	enc.PutUint64(i.mmap[i.size+offWidth:i.size+entWidth], pos)
+	i.size += uint64(entWidth)
+	return nil
+}
+
+func (i *index) Name() string {
+
+	return i.file.Name()
 
 }
